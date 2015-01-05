e2ed3fa7a7b9dba0234a40ce5a696eac2a503c05
Amit Patel <CHETU\amitp@chetuiwk430.chetu.com>
2015-01-05 18:35:06 +0530

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+  layout 'bootstrap'  
+  def home
+  end
+
+  def help
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearningApp::Application.config.secret_key_base = '37cc2c92f4260aade9a1e1272d0574d996e9fb26a3a2f9e8d8d9119a809500ac8c50ce155eebcafa5b1d71a81af8f19b213a9c78f6d7369acf140c55b476f488'
